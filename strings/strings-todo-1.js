3d40ab0905da7f3c119f180e8dfc2abc7b6783bf
Wyatt Ptak <ptakwyatt@gmail.com>
2024-04-28 19:04:46 -0700

    added getDigits fn

--- strings/strings-todo-1.js
+++ strings/strings-todo-1.js
@@ -36,6 +36,17 @@ const removeBlanks = (str) => {
 //
 // getDigits("0s1a3y5w7h9a2t4?6!8?0") => 1357924680
 
+const getDigits = (str) => {
+  let numbers = ""
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i])) {
+      numbers += str[i]
+    }
+  }
+  return Number(numbers)
+
+}
+// console.log(getDigits("jkl342j3kl4j23lhb523o"))
 
 // ----------------------
 
