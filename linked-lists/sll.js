c0e6e6c95357ddbad72b90aa638871b745414dfd
Wyatt Ptak <ptakwyatt@gmail.com>
2024-05-09 07:42:47 -0700

    added frontal usage of SLL

---
+++ linked-lists/sll.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SLL {
+  constructor() {
+    this.head = null;
+  }
+
+  front() {
+    if (!this.head) {
+      return null;
+    }
+    return this.head.data;
+  }
+
+  addFront(value) {
+    let new_node = new Node(value);
+
+    if (!this.head) {
+      this.head = new_node;
+      return this;
+    }
+
+    new_node.next = this.head;
+    this.head = new_node;
+    return this;
+  }
+
+  removeFront() {
+    if (!this.head) {
+      return null;
+    }
+    this.head = this.head.next;
+  }
+}
+
+let SLL1 = new SLL();
+SLL1.addFront(18);
+SLL1.addFront(5);
+SLL1.addFront(73);
+
+console.log(SLL1.head);
+console.log("Next: " + SLL1.head.next);
+
+SLL1.removeFront();
+
+console.log(SLL1.head);
+console.log(SLL1.front())
